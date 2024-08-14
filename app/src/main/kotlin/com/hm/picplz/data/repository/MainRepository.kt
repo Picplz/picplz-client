3133676e4fe51c62a192e531d3e4d2acc35cbc83
ldhldh07 <dhlim07@naver.com>
2024-08-14 17:14:57 +0900

    :sparkles: : 임시 user 정보 호출 동작 설정 - 스플래시 화면 동작에 연동 - 임시로 더미데이터를 3초 후에 호출하는 방식으로 설정

--- app/src/main/kotlin/com/hm/picplz/data/repository/MainRepository.kt
+++ app/src/main/kotlin/com/hm/picplz/data/repository/MainRepository.kt
@@ -1,6 +1,37 @@
 package com.hm.picplz.data.repository
 
-import com.hm.picplz.data.service.UserService
+import android.util.Log
+import com.hm.picplz.data.model.User
+import com.hm.picplz.data.source.UserDataSource
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.filterNotNull
+import javax.inject.Inject
 
-class MainRepository(private val apiService: UserService) {
+class MainRepository @Inject constructor(
+    private val userDataSource: UserDataSource
+) {
+
+    private val _userData = MutableSharedFlow<User>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+
+    private val currentUserData get() = _userData.replayCache.firstOrNull() ?: emptyUserData
+
+    val userData: Flow<User> = _userData.filterNotNull()
+
+
+    suspend fun fetchUserData() {
+        Log.d("MainRepository", "Fetching user data...")
+        val fetchedUserData = userDataSource.getUserData()
+        _userData.emit(fetchedUserData)
+        Log.d("MainRepository", "User data emitted: $fetchedUserData")
+    }
+
+    companion object {
+        val emptyUserData = User(
+            id = 0,
+            name = "Unknown",
+            email = "unknown@example.com"
+        )
+    }
 }
--- app/src/main/kotlin/com/hm/picplz/data/source/UserDataSource.kt
+++ app/src/main/kotlin/com/hm/picplz/data/source/UserDataSource.kt
@@ -2,9 +2,16 @@ package com.hm.picplz.data.source
 
 import com.hm.picplz.data.model.User
 import com.hm.picplz.data.service.UserService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class UserDataSource(private val userService: UserService) {
-    suspend fun fetchUsers(): List<User> {
-        return userService.getUsers()
+class UserDataSource @Inject constructor(
+    private val userService: UserService
+) {
+    suspend fun getUserData(): User {
+        return withContext(Dispatchers.IO) {
+            userService.getUser()
+        }
     }
 }
