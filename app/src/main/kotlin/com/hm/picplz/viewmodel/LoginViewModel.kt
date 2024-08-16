c5f54c261b20eee17d6b4cf3155c79d056839ee7
ldhldh07 <dhlim07@naver.com>
2024-08-16 20:43:18 +0900

    :sparkles: : 소셜 로그인 이동을 사이드 이펙트로 설정 - 기본 사이드 이펙트 파일 및 기본 코드 추가 - intent를 발동하면 side-effect를 발동시키고 ui파일에 관련 코드가 있어 emit한 동작을 관찰하여 동작

--- app/src/main/kotlin/com/hm/picplz/viewmodel/LoginViewModel.kt
+++ app/src/main/kotlin/com/hm/picplz/viewmodel/LoginViewModel.kt
@@ -2,7 +2,9 @@ package com.hm.picplz.ui.screen.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
@@ -11,15 +13,16 @@ class LoginViewModel : ViewModel() {
     private val _state = MutableStateFlow<LoginState>(LoginState.Idle)
     val state: StateFlow<LoginState> get() = _state
 
+    private val _sideEffect = MutableSharedFlow<LoginSideEffect>()
+    val sideEffect: SharedFlow<LoginSideEffect> get() = _sideEffect
+
     fun handleIntent(intent: LoginIntent) {
         when (intent) {
-            is LoginIntent.NavigateToKaKao -> navigateToKaKao()
-        }
-    }
-
-    private fun navigateToKaKao() {
-        viewModelScope.launch {
-            // 소셜 로그인 관련 로직 추가
+            is LoginIntent.NavigateToKaKao -> {
+                viewModelScope.launch {
+                    _sideEffect.emit(LoginSideEffect.NavigateToKaKao)
+                }
+            }
         }
     }
 }
