9a930d6094fa0234d8b404d809cddcd8ccb6509b
ldhldh07 <dhlim07@naver.com>
2024-08-16 16:10:52 +0900

    :sparkles: : 회원가입 화면 기본 파일 추가

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpScreen.kt
@@ -0,0 +1,11 @@
+package com.hm.picplz.ui.screen.sign_up
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hm.picplz.viewmodel.SignUpViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier, viewModel: SignUpViewModel = viewModel()) {
+
+}
---
+++ app/src/main/kotlin/com/hm/picplz/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.hm.picplz.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class SignUpViewModel : ViewModel() {
+}
