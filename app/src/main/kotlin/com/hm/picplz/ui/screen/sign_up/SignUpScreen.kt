73803a3ec0828fb21e7a625bcb4ef1e0070947bc
ldhldh07 <dhlim07@naver.com>
2024-08-17 15:23:42 +0900

    :adhesive_bandage: : import문 오기입 수정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpScreen.kt
@@ -8,15 +8,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.hm.picplz.viewmodel.SignUpViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.hm.picplz.ui.theme.PicplzTheme
 
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier,
     viewModel: SignUpViewModel = viewModel(),
-    navController: NavHostController
+    navController: NavController,
 ) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Box(
@@ -24,8 +27,17 @@ fun SignUpScreen(
                 .fillMaxSize()
                 .padding(innerPadding),
             contentAlignment = Alignment.Center
-        ){
-            Text("회원 설정 페이지")
+        ) {
+            Text("손님 금손 선택")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview() {
+    val navController = rememberNavController()
+    PicplzTheme {
+        SignUpScreen(navController = navController)
     }
 }
