7189dd91ee61210ce04ba991dd3e37cf66345d1b
ldhldh07 <dhlim07@naver.com>
2024-08-17 14:47:25 +0900

    :lipstick: : 회원 정보 입력 페이지 기본 파일 추가

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photograppher/SignUpPhotographerScreen.kt
@@ -0,0 +1,63 @@
+package com.hm.picplz.ui.screen.sign_up_client
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun SignUpPhtographerScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    Scaffold (
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("금손 설정 페이지")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+
+            }
+            Box(
+                modifier = Modifier.height(30.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = {
+                        /*TODO*/
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Blue,
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                ) {
+                    Text(text = "다음")
+                }
+            }
+
+        }
+    }
+}
