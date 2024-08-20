144940747744a57bc59d6a192d43c461f1d2b5c8
ldhldh07 <dhlim07@naver.com>
2024-08-20 17:09:35 +0900

    :construction_worker: : 테마 색깔 지정

--- app/src/main/kotlin/com/hm/picplz/ui/theme/Color.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.hm.picplz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+object MainThemeColor {
+    val Olive = Color(0xFFA6CB80)
+    val Black = Color(0xFF020202)
+    val White = Color(0xFFFEFEFE)
+    val Gray = Color(0xFF949494)
+}
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
