82b429a70c35ed9a0114079ccba7e9dce522926d
ldhldh07 <dhlim07@naver.com>
2024-08-20 21:30:40 +0900

    :lipstick: : 간단한 ui 적용 - 상단부 - 상단 뒤로가기 화살표 및 텍스트 스타일 변경

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpScreen.kt
@@ -1,26 +1,10 @@
 package com.hm.picplz.ui.screen.sign_up
 
 import android.os.Bundle
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -29,8 +13,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.os.bundleOf
 import androidx.core.view.WindowCompat
 import com.hm.picplz.viewmodel.SignUpViewModel
@@ -40,6 +28,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 import androidx.navigation.compose.rememberNavController
 import com.hm.picplz.MainActivity
+import com.hm.picplz.R
 import com.hm.picplz.data.model.UserType
 import com.hm.picplz.ui.theme.PicplzTheme
 import kotlinx.coroutines.flow.collectLatest
@@ -57,12 +46,13 @@ fun SignUpScreen(
 
     LaunchedEffect(Unit) {
         activity?.window?.apply {
-            statusBarColor = Color.Gray.toArgb()
-            WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = false
+            statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
         }
     }
 
-    val selectedUserType = viewModel.selectedUserType.collectAsState().value
+    val currentState = viewModel.state.collectAsState().value
+    val selectedUserType = currentState.selectedUserType
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
@@ -74,14 +64,15 @@ fun SignUpScreen(
         ) {
             Box(
                 modifier = Modifier
-                    .height(100.dp)
-                    .fillMaxWidth()
-                    .background(Color.Gray),
+                    .height(50.dp)
+                    .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     IconButton(
                         onClick = {
@@ -89,20 +80,29 @@ fun SignUpScreen(
                         },
                         modifier = Modifier
                             .fillMaxHeight()
+                            .padding(start = 10.dp)
                     ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back",
-                            modifier = Modifier.padding(8.dp)
+                        Image(
+                            painter = painterResource(R.drawable.arrow_left),
+                            contentDescription = "arrow left",
+                            modifier = Modifier.size(35.dp)
                         )
                     }
                     Box(
                         modifier = Modifier
+                            .weight(1f)
                             .fillMaxHeight(),
                         contentAlignment = Alignment.Center
-                    ){
-                        Text("회원 타입 선텍")
+                    ) {
+                        Text(
+                            text = "회원 타입 선택",
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
                     }
+                    Spacer(modifier = Modifier.width(60.dp))
                 }
             }
             Box(
@@ -118,7 +118,7 @@ fun SignUpScreen(
                     verticalArrangement = Arrangement.Center
                 ) {
                     Text(text = "선택해주세요")
-                    Row(
+                    Column(
                         modifier = Modifier,
                     ) {
                         Button(
@@ -155,7 +155,7 @@ fun SignUpScreen(
                     ),
                     enabled = selectedUserType != null
                 ) {
-                 Text("다음")
+                    Text("다음")
                 }
             }
         }
