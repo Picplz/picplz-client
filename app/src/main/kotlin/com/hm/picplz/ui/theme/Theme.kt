aeef1c6821a1e6376ea4d3d3e9d039f4fa4880b3
ldhldh07 <dhlim07@naver.com>
2024-08-20 16:08:55 +0900

    :construction_worker: : 기본 폰트 프리텐다드 설정

--- app/src/main/kotlin/com/hm/picplz/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/theme/Theme.kt
@@ -1,15 +1,18 @@
 package com.hm.picplz.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -52,7 +55,7 @@ fun PicplzTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = pretendardTypography,
         content = content
     )
 }
