aa26b701f30c1f846873d1de48ca6f1f23637283
ldhldh07 <dhlim07@naver.com>
2024-08-21 20:13:12 +0900

    :recycle: : 닉네임 설정 단계 view 분리 - View 파일로 분리 후 state의 step에 따라서 출력 - 3개의 영역으로 레이아웃 설정   - Common 상단바가 있는 상단부   - 입력이 들어갈 중간 부분   - 버튼이 들어갈 하단부

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientNicknameView.kt
@@ -0,0 +1,55 @@
+package com.hm.picplz.ui.screen.sign_up_client
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.hm.picplz.ui.screen.common.CommonButton
+import com.hm.picplz.ui.screen.common.CommonTopBar
+import com.hm.picplz.viewmodel.SignUpClientViewModel
+import com.hm.picplz.ui.screen.sign_up.SignUpClientState
+import com.hm.picplz.ui.screen.sign_up.SignUpIntent.NavigateToSelected
+import com.hm.picplz.ui.theme.MainThemeColor
+
+@Composable
+fun SignUpClientNicknameView(
+    modifier: Modifier = Modifier,
+    currentState: SignUpClientState,
+    viewModel: SignUpClientViewModel,
+    innerPadding: PaddingValues
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CommonTopBar(
+            text ="닉네임 등록하기",
+            onClickBack = { viewModel.handleIntent(SignUpClientIntent.NavigateToPrev) },
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            contentAlignment = Alignment.Center
+        ) {}
+        Box(
+            modifier = Modifier
+                .height(120.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {}
+    }
+}
--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientScreen.kt
@@ -1,19 +1,14 @@
 package com.hm.picplz.ui.screen.sign_up_client
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.hm.picplz.data.model.User
-import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.viewmodel.SignUpClientViewModel
 import com.hm.picplz.viewmodel.emptyUserData
@@ -28,26 +23,24 @@ fun SignUpClientScreen(
 ) {
     val currentState = viewModel.state.collectAsState().value
 
-    Scaffold (
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
         containerColor = MainThemeColor.White
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            CommonTopBar(
-                text =
-                    when(currentState.currentStep) {
-                        0 -> "닉네임 등록하기"
-                        1 -> "프로필 이미지 업로드"
-                        else -> ""
-                    },
-                onClickBack = { viewModel.handleIntent(SignUpClientIntent.NavigateToPrev) },
+        when (currentState.currentStep) {
+            0 -> SignUpClientNicknameView(
+                modifier = modifier,
+                currentState = currentState,
+                viewModel = viewModel,
+                innerPadding = innerPadding
             )
+//            1 -> SignUpClientProfileImageView(
+//                modifier = modifier,
+//                currentState = currentState,
+//                viewModel = viewModel,
+//                innerPadding = innerPadding
+//            )
+            else -> {}
         }
     }
 
