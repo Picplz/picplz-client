4d7dd8f879f7eed63292f40f580f7712c45cfc12
ldhldh07 <dhlim07@naver.com>
2024-08-21 16:22:46 +0900

    :adhesive_bandage: : data class 변수명 수정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpState.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpState.kt
@@ -2,14 +2,14 @@ package com.hm.picplz.ui.screen.sign_up
 
 import com.hm.picplz.data.model.UserType
 
-data class SignUpViewState(
+data class SignUpState(
     val selectedUserType: UserType? = null,
     val isLoading: Boolean = false,
     val error: Throwable? = null
 ) {
     companion object {
-        fun idle(): SignUpViewState {
-            return SignUpViewState(
+        fun idle(): SignUpState {
+            return SignUpState(
                 selectedUserType = null,
                 isLoading = false,
                 error = null
