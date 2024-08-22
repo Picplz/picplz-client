8b1d07dcf3d34b5674a47c9305baa9a4c79df1e8
ldhldh07 <dhlim07@naver.com>
2024-08-22 15:17:40 +0900

    :sparkles: : 프로필 업로드 페이지 프리뷰 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientProfileImageView.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientProfileImageView.kt
@@ -2,11 +2,15 @@ package com.hm.picplz.ui.screen.sign_up_client
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.screen.sign_up.SignUpClientState
+import com.hm.picplz.ui.theme.PicplzTheme
 import com.hm.picplz.viewmodel.SignUpClientViewModel
 
 @Composable
@@ -41,5 +45,19 @@ fun SignUpClientProfileImageView (
                 .padding(horizontal = 16.dp),
             contentAlignment = Alignment.Center
         ) {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpClientProfileImageViewPreview () {
+    val viewModel: SignUpClientViewModel = viewModel()
+    val currentState = viewModel.state.collectAsState().value
+    PicplzTheme {
+        SignUpClientProfileImageView(
+            currentState = currentState,
+            viewModel = viewModel,
+            innerPadding = PaddingValues() ,
+        )
     }
 }
