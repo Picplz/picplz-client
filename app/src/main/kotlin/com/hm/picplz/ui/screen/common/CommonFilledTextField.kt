a125724c371470bc23f17f10f580e6b4e7cba499
ldhldh07 <dhlim07@naver.com>
2024-08-22 11:09:29 +0900

    :lipstick: : 공통 filled textfield 제작 - 하단 밑줄만 나오는 텍스트 필드

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonFilledTextField.kt
@@ -0,0 +1,96 @@
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hm.picplz.ui.theme.MainThemeColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonFilledTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "",
+    isError: Boolean = false,
+    supportingText: String? = null,
+    imeAction: ImeAction = ImeAction.Done,
+    keyboardActions: () -> Unit = {},
+    visualTransformation : VisualTransformation = VisualTransformation.None,
+    singleLine: Boolean = true,
+    enabled: Boolean = true
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val colors = OutlinedTextFieldDefaults.colors(
+        focusedBorderColor = MainThemeColor.Olive,
+        unfocusedBorderColor = MainThemeColor.Gray,
+        focusedLabelColor = MainThemeColor.Olive,
+        unfocusedLabelColor = MainThemeColor.Gray,
+        focusedPlaceholderColor = Color.Gray,
+        unfocusedPlaceholderColor = Color.Gray
+    )
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        singleLine = true,
+        interactionSource = interactionSource,
+        enabled = true,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = imeAction
+        ),
+        keyboardActions = KeyboardActions(
+            onAny = {
+                keyboardActions()
+            }
+        )
+    ) {
+        TextFieldDefaults.DecorationBox(
+            value = value,
+            visualTransformation = visualTransformation,
+            innerTextField = it,
+            singleLine = singleLine,
+            enabled = enabled,
+            interactionSource = interactionSource,
+            placeholder = {
+                if (placeholder.isNotEmpty()) {
+                    Text(
+                        text = placeholder,
+                        fontSize = 16.sp,
+                        color = Color.Gray
+                    )
+                }
+            },
+            supportingText = {
+                if (supportingText != null) {
+                    Text(
+                        text = supportingText,
+                        fontSize = 12.sp,
+                        color = if (isError) Color.Red else Color.Gray,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+            },
+            contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(
+                start = 4.dp, bottom = 0.dp
+            ),
+            colors = colors
+        )
+    }
+}
