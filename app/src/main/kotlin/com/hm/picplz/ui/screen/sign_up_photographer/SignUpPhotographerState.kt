9d53b486ea3b1fb4f1f61c0add971ade60ea5cb6
ldhldh07 <dhlim07@naver.com>
2024-08-27 19:48:36 +0900

    :sparkles: : 금손 회원 정보 State 설정

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerState.kt
@@ -0,0 +1,28 @@
+package com.hm.picplz.ui.screen.sign_up_photographer
+
+import android.net.Uri
+import com.hm.picplz.data.model.User
+import com.hm.picplz.viewmodel.emptyUserData
+
+class SignUpPhotographerState (
+    val currentStep: Int? = 0,
+    val isLoading: Boolean = false,
+    val error: Throwable? = null,
+    val userInfo: User = emptyUserData,
+    val nickname: String = "",
+    val profileImageUri: Uri? = null,
+) {
+    companion object {
+        fun idle(): SignUpPhotographerState {
+            return SignUpPhotographerState(
+                currentStep = 0,
+                isLoading = false,
+                error = null,
+                userInfo = emptyUserData,
+                nickname = "",
+                profileImageUri = null,
+            )
+        }
+    }
+
+}
