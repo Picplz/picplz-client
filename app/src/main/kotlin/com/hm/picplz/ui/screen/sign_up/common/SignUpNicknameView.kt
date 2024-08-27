b1f10624c190addf7efcf84b663903d0dfa8fa7c
ldhldh07 <dhlim07@naver.com>
2024-08-27 19:02:01 +0900

    :sparkles: : 닉네임 입력하기 페이지 재사용 가능하도록 모듈화 - params : 상태 및 세팅, 뒤로가기 버튼 클릭, 다음 버튼 클릭,

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/common/SignUpNicknameView.kt
@@ -0,0 +1,123 @@
+package com.hm.picplz.ui.screen.sign_up.common
+
+import CommonFilledTextField
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hm.picplz.ui.screen.common.CommonButton
+import com.hm.picplz.ui.screen.common.CommonTopBar
+import com.hm.picplz.ui.theme.MainThemeColor
+
+@Composable
+fun SignUpNicknameView(
+    modifier: Modifier = Modifier,
+    currentNickname: String,
+    innerPadding: PaddingValues,
+    onNavigateToPrev: () -> Unit,
+    onNicknameChange: (String) -> Unit,
+    onNextStep: () -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .imePadding()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CommonTopBar(
+            text = "닉네임 등록하기",
+            onClickBack = { onNavigateToPrev() },
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = {
+                        focusManager.clearFocus()
+                    })
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        append("닉네임을\n")
+                        append("선택해주세요")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+                CommonFilledTextField(
+                    value = currentNickname,
+                    onValueChange = { newValue ->
+                        onNicknameChange(newValue)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = "닉네임을 입력하세요",
+                    isError = false,
+                    imeAction = ImeAction.Done,
+                    keyboardActions = {
+                        focusManager.clearFocus()
+                    },
+                    label = "닉네임"
+                )
+                Spacer(
+                    modifier = Modifier.height(80.dp)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .height(120.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CommonButton(
+                text = "다음",
+                onClick = { onNextStep() },
+                enabled = currentNickname.isNotEmpty(),
+                containerColor = MainThemeColor.Black
+            )
+        }
+    }
+}
