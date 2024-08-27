d3e3dda26c2a311d232ed6da276ca765c1eb7f25
ldhldh07 <dhlim07@naver.com>
2024-08-27 19:48:46 +0900

    :sparkles: : 금손 회원 정보 Intent 설정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerIntent.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerIntent.kt
@@ -1,6 +1,13 @@
 package com.hm.picplz.ui.screen.sign_up_photographer
 
+import android.net.Uri
+import com.hm.picplz.data.model.User
 
 sealed class SignUpPhotographerIntent {
+    data class SetUserInfo(val userInfo: User) : SignUpPhotographerIntent()
+    data class SetNickname(val newNickname: String) : SignUpPhotographerIntent()
+    data class SetProfileImageUri(val newProfileImageUri: Uri?) : SignUpPhotographerIntent()
     data object NavigateToPrev : SignUpPhotographerIntent()
+    data class ChangeStep(val stepNum: Int) : SignUpPhotographerIntent()
+    data object ClickSubmitButton : SignUpPhotographerIntent()
 }
