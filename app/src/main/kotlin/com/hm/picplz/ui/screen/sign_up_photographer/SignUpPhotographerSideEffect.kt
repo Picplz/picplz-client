00f909940f74bef940e2c3913ab4d4af925f0fe0
ldhldh07 <dhlim07@naver.com>
2024-08-28 17:54:00 +0900

    :sparkles: : 금손 세팅 스크린 사이드 이펙트 정의 - 프로필 제출하는 side effect - 파일 업로드 다이얼로그 여는 side effect

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerSideEffect.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerSideEffect.kt
@@ -1,6 +1,13 @@
 package com.hm.picplz.ui.screen.sign_up_photographer
 
+import android.net.Uri
+
 
 sealed class SignUpPhotographerSideEffect {
+    data class  SubmitProfileInfo(
+        val nickname: String,
+        val profileImageUri: Uri?,
+    ) : SignUpPhotographerSideEffect()
+    data object ShowFileUploadDialog : SignUpPhotographerSideEffect()
     data object NavigateToPrev : SignUpPhotographerSideEffect()
 }
