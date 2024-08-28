c01ca1c489ed7187e87cdbccc555275a7021ec5b
ldhldh07 <dhlim07@naver.com>
2024-08-28 10:47:14 +0900

    :lipstick: : 금손 닉네임 제작 스크린 - 닉네임뷰 재사용 composable 사용

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerNicknameView.kt
@@ -0,0 +1,24 @@
+package com.hm.picplz.ui.screen.sign_up_photographer
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hm.picplz.ui.screen.sign_up.common.SignUpNicknameView
+import com.hm.picplz.viewmodel.SignUpPhotographerViewModel
+
+@Composable
+fun SignUpPhotographerNicknameView (
+    modifier: Modifier = Modifier,
+    currentState: SignUpPhotographerState,
+    viewModel: SignUpPhotographerViewModel,
+    innerPadding: PaddingValues
+) {
+    SignUpNicknameView(
+        modifier = modifier,
+        currentNickname = currentState.nickname,
+        onNavigateToPrev = { viewModel.handleIntent(SignUpPhotographerIntent.NavigateToPrev) },
+        onNicknameChange = { newValue -> viewModel.handleIntent(SignUpPhotographerIntent.SetNickname(newValue)) },
+        onNextStep = { viewModel.handleIntent(SignUpPhotographerIntent.ChangeStep(1))},
+        innerPadding = innerPadding,
+    )
+}
--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_photographer/SignUpPhotographerScreen.kt
@@ -1,46 +1,38 @@
 package com.hm.picplz.ui.screen.sign_up_photographer
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.hm.picplz.data.model.User
-import com.hm.picplz.ui.screen.common.CommonTopBar
-import com.hm.picplz.ui.screen.sign_up_client.SignUpClientIntent
-import com.hm.picplz.ui.screen.sign_up_client.SignUpClientSideEffect
 import com.hm.picplz.ui.theme.MainThemeColor
-import com.hm.picplz.viewmodel.SignUpClientViewModel
 import com.hm.picplz.viewmodel.SignUpPhotographerViewModel
+import com.hm.picplz.viewmodel.emptyUserData
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun SignUpPhotographerScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
-    userInfo: User,
+    userInfo: User = emptyUserData,
     viewModel: SignUpPhotographerViewModel = viewModel(),
-    ) {
+) {
+    val currentState = viewModel.state.collectAsState().value
+
     Scaffold (
         modifier = Modifier.fillMaxSize(),
         containerColor = MainThemeColor.White
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            CommonTopBar(
-                text = "활동 지역 선택",
-                onClickBack = { viewModel.handleIntent(SignUpPhotographerIntent.NavigateToPrev) },
+        when (currentState.currentStep) {
+            0 -> SignUpPhotographerNicknameView(
+                modifier = modifier,
+                currentState = currentState,
+                viewModel = viewModel,
+                innerPadding = innerPadding,
             )
         }
     }
