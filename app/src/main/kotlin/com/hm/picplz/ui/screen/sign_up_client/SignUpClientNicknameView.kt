87ef6e38de96a58acaf22ed69296b3918f411695
ldhldh07 <dhlim07@naver.com>
2024-08-28 13:50:45 +0900

    :fire: : 미사용 import 삭제

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientNicknameView.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up_client/SignUpClientNicknameView.kt
@@ -1,26 +1,12 @@
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.hm.picplz.ui.screen.common.CommonButton
-import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.viewmodel.SignUpClientViewModel
 import com.hm.picplz.ui.screen.sign_up.SignUpClientState
 import com.hm.picplz.ui.screen.sign_up.common.SignUpNicknameView
 import com.hm.picplz.ui.screen.sign_up_client.SignUpClientIntent
-import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
 
 @Composable
