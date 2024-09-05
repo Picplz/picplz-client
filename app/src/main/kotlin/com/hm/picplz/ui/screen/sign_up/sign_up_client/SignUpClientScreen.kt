ab3b5a0052437ff4cc4f576c0b47034a01d48b90
ldhldh07 <dhlim07@naver.com>
2024-09-05 13:49:16 +0900

    :recycle: : 고객 회원가입 영역에서 사라진 부분 조정 - 파일 삭제및 사용한 부분 삭제

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_client/SignUpClientScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_client/SignUpClientScreen.kt
@@ -1,17 +1,21 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_client
 
-import SignUpClientNicknameView
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.hm.picplz.data.model.User
+import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.viewmodel.SignUpClientViewModel
 import com.hm.picplz.viewmodel.emptyUserData
@@ -30,20 +34,17 @@ fun SignUpClientScreen(
         modifier = Modifier.fillMaxSize(),
         containerColor = MainThemeColor.White
     ) { innerPadding ->
-        when (currentState.currentStep) {
-            0 -> SignUpClientNicknameView(
-                modifier = modifier,
-                currentState = currentState,
-                viewModel = viewModel,
-                innerPadding = innerPadding
+        Column (
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CommonTopBar(
+                text = "고객 선택",
+                onClickBack = {}
             )
-            1 -> SignUpClientProfileImageView(
-                modifier = modifier,
-                currentState = currentState,
-                viewModel = viewModel,
-                innerPadding = innerPadding
-            )
-            else -> {}
         }
     }
     val context = LocalContext.current
