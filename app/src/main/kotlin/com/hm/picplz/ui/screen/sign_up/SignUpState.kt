6520b952a2606f1fd9afb1d3e649a03c571d050b
ldhldh07 <dhlim07@naver.com>
2024-09-05 10:59:39 +0900

    :recycle: : signUpState 업데이트 - 해당 화면에서 닉네임 및 프로필 이미지 관리

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpState.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/SignUpState.kt
@@ -1,18 +1,25 @@
 package com.hm.picplz.ui.screen.sign_up
 
+import android.net.Uri
 import com.hm.picplz.data.model.UserType
 
 data class SignUpState(
+    val currentStep: Int = 0,
     val selectedUserType: UserType? = null,
     val isLoading: Boolean = false,
-    val error: Throwable? = null
+    val error: Throwable? = null,
+    val nickname: String = "",
+    val profileImageUri: Uri? = null
 ) {
     companion object {
         fun idle(): SignUpState {
             return SignUpState(
+                currentStep = 0,
                 selectedUserType = null,
                 isLoading = false,
-                error = null
+                error = null,
+                nickname = "",
+                profileImageUri = null,
             )
         }
     }
