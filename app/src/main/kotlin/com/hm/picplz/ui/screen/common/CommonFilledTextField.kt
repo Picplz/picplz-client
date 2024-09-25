0ec010a5ca6982e78005ed6e8a8be34d3616af32
ldhldh07 <dhlim07@naver.com>
2024-09-25 21:07:41 +0900

    :lipstick: : CommonFilledTextField ui 적용 - 기획된 텍스트 필드 디자인 적용

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonFilledTextField.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonFilledTextField.kt
@@ -1,27 +1,34 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hm.picplz.data.model.NicknameFieldError
 import com.hm.picplz.ui.theme.MainThemeColor
+import com.hm.picplz.ui.theme.PicplzTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -36,12 +43,13 @@ fun CommonFilledTextField(
     keyboardActions: () -> Unit = {},
     visualTransformation: VisualTransformation = VisualTransformation.None,
     singleLine: Boolean = true,
-    enabled: Boolean = true
+    enabled: Boolean = true,
+    shape: RoundedCornerShape = RoundedCornerShape(5.dp)
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val colors = OutlinedTextFieldDefaults.colors(
-        focusedBorderColor = MainThemeColor.Olive,
-        unfocusedBorderColor = MainThemeColor.Gray,
+        focusedBorderColor = Color.Transparent,
+        unfocusedBorderColor = Color.Transparent,
         focusedLabelColor = MainThemeColor.Gray,
         unfocusedLabelColor = MainThemeColor.Gray,
         focusedPlaceholderColor = Color.Gray,
@@ -60,12 +68,19 @@ fun CommonFilledTextField(
                 modifier = Modifier.padding(bottom = 4.dp)
             )
         }
-
         BasicTextField(
             value = value,
             onValueChange = onValueChange,
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(50.dp)
+                .clip(shape)
+                .background(MainThemeColor.Gray2)
+                .border(
+                    width = 0.dp,
+                    color = Color.Transparent,
+                    shape = shape
+                ),
             singleLine = true,
             interactionSource = interactionSource,
             enabled = enabled,
@@ -76,6 +91,9 @@ fun CommonFilledTextField(
                 onAny = {
                     keyboardActions()
                 }
+            ),
+            textStyle = MaterialTheme.typography.bodyLarge.copy(
+                color = MainThemeColor.Black
             )
         ) {
             TextFieldDefaults.DecorationBox(
@@ -89,28 +107,36 @@ fun CommonFilledTextField(
                     if (placeholder.isNotEmpty()) {
                         Text(
                             text = placeholder,
-                            fontSize = 16.sp,
+                            style = MaterialTheme.typography.bodyLarge,
                             color = Color.Gray
                         )
                     }
                 },
-                supportingText = {
-                    if (isError) {
-                        Text(
-                            text = errors.first().message,
-                            fontSize = 12.sp,
-                            color = Color.Red,
-                            modifier = Modifier.padding(top = 4.dp)
-                        )
-                    } else {
-                        Spacer(modifier = Modifier.height(18.dp))
-                    }
-                },
-                contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(
-                    start = 4.dp, bottom = 0.dp
-                ),
                 colors = colors
             )
         }
+
+        if (isError) {
+            Text(
+                text = "* ${errors.first().message}",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                lineHeight = 12.sp * 1.4,
+                color = Color(0xFFFF0000),
+                modifier = Modifier.padding(top = 4.dp, start = 4.dp)
+            )
+        } else {
+            Spacer(modifier = Modifier.height(21.dp))
+        }
     }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun CommonFilledTextFieldPreview() {
+    PicplzTheme {
+        CommonFilledTextField(value = "value", onValueChange = {})
+    }
+}
+
