efac1514a9ddab2c381d758bf9af7fdad1c59c51
ldhldh07 <dhlim07@naver.com>
2024-09-26 22:21:22 +0900

    :lipstick: : 고객 타입 선택 버튼 ui 적용 - 상태 타입 3개로 조정 - 이미지 및 라벨 배치 조정 - 버튼이 아닌 clickable 이미지로 설정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonSelectImageButton.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonSelectImageButton.kt
@@ -1,92 +1,76 @@
 package com.hm.picplz.ui.screen.common
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.hm.picplz.R
 
+enum class SelectionState {
+    UNSELECTED, SELECTED, DESELECTED
+}
 
 @Composable
 fun CommonSelectImageButton(
     text: String,
-    isSelected: Boolean,
+    selectionState: SelectionState,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    @androidx.annotation.DrawableRes iconResId: Int? = null,
-    selectedColor: Color = MainThemeColor.Olive,
-    unselectedColor: Color = Color.Transparent,
-    backgroundColor: Color = Color.White,
+    @DrawableRes unSelectedIconResId: Int? = null,
+    @DrawableRes deSelectedIconResId: Int? = null,
+    @DrawableRes selectedIconResId: Int? = null,
     contentColor: Color = MainThemeColor.Black,
-    elevation: Dp = 5.dp,
-    shape: RoundedCornerShape = RoundedCornerShape(16.dp)
 ) {
-    Button(
-        onClick = onClick,
+    val iconResId = when (selectionState) {
+        SelectionState.UNSELECTED -> unSelectedIconResId
+        SelectionState.DESELECTED -> deSelectedIconResId
+        SelectionState.SELECTED -> selectedIconResId
+    }
+
+    Box(
         modifier = modifier
-            .fillMaxWidth()
-            .height(100.dp)
-            .shadow(
-                elevation = elevation,
-                shape = shape,
-                ambientColor = Color.Gray,
-                spotColor = Color.Gray
-            )
-            .background(
-                color = backgroundColor,
-                shape = shape
-            )
-            .border(
-                width = 2.dp,
-                color = if (isSelected) selectedColor else unselectedColor,
-                shape = shape
-            ),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = backgroundColor,
-            contentColor = contentColor
-        ),
-        shape = shape,
+            .width(if (selectionState == SelectionState.SELECTED) 160.dp else 120.dp)
+            .height(if (selectionState == SelectionState.SELECTED) 190.dp else 150.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            if (iconResId != null) {
+            iconResId?.let { iconResId ->
                 Image(
                     painter = painterResource(id = iconResId),
-                    contentDescription = null,
-                    modifier = Modifier.size(40.dp)
+                    contentDescription = "User Type Button",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(if (selectionState == SelectionState.SELECTED) 160.dp else 120.dp)
+                        .clickable { onClick() }
                 )
-                Spacer(modifier = Modifier.width(20.dp))
             }
             Text(
                 text = text,
@@ -108,9 +92,11 @@ fun CommonSelectImageButtonPreview() {
     PicplzTheme {
         CommonSelectImageButton(
             text = "라벨",
-            isSelected = false,
+            selectionState = SelectionState.UNSELECTED,
             onClick = { isSelected = !isSelected },
-            iconResId = R.drawable.logo
+            unSelectedIconResId = R.drawable.photographer_unselected,
+            deSelectedIconResId = R.drawable.photographer_deselected,
+            selectedIconResId = R.drawable.photographer_selected
         )
     }
 }
