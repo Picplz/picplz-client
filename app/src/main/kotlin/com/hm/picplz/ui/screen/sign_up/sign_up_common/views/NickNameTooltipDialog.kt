95f298654d3d25e6d9f5b9abf9de290660d7445e
ldhldh07 <dhlim07@naver.com>
2024-09-27 10:03:59 +0900

    :sparkles: : 툴팁 다이알로그 변경해서 공통 텍스트 다이알로그 composable 구현

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_common/views/NickNameTooltipDialog.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_common/views/NickNameTooltipDialog.kt
@@ -1,7 +1,7 @@
-package com.hm.picplz.ui.screen.sign_up.sign_up_common.views
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,72 +9,75 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hm.picplz.R
-import com.hm.picplz.ui.screen.sign_up.sign_up_common.SignUpCommonIntent
 import com.hm.picplz.ui.theme.MainThemeColor
-import com.hm.picplz.ui.theme.PicplzTheme
-import com.hm.picplz.viewmodel.SignUpCommonViewModel
 
 @Composable
-fun NicknameTooltipDialog (
-    onDismissRequest: () -> Unit
-){
+fun CommonTextDialog(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+    dialogText: String,
+    dialogTextSize: Int = 12,
+    textPadding: Dp = 10.dp,
+    dialogShape: RoundedCornerShape = RoundedCornerShape(16.dp),
+    dialogElevation: Dp = 8.dp,
+    backgroundColor: Color = MainThemeColor.White,
+) {
     Dialog(
         onDismissRequest = { onDismissRequest() }
     ) {
         Surface(
-            shape = RoundedCornerShape(16.dp),
-            color = MainThemeColor.White,
-            tonalElevation = 8.dp,
-            modifier = Modifier
+            shape = dialogShape,
+            color = backgroundColor,
+            tonalElevation = dialogElevation,
+            modifier = modifier
                 .padding(16.dp),
         ) {
             Row(
                 modifier = Modifier
-                    .padding(8.dp)
-            ){
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
                 Text(
                     modifier = Modifier
-                        .padding(10.dp),
+                        .padding(textPadding),
                     text = buildAnnotatedString {
-                        append("∙ 한글, 영문, 숫자 입력 가능 (2~15자)\n")
-                        append("∙ 중복 닉네임은 불가\n")
-                        append("∙ 이모티콘, 특수문자 사용이 불가\n")
-                        append("∙ 닉네임의 처음과 마지막 부분 공백 사용 불가")
-                    },                        style = TextStyle(
-                        fontSize = 12.sp
+                        append(dialogText)
+                    },
+                    style = TextStyle(
+                        fontSize = dialogTextSize.sp
                     ),
                 )
                 Image(
                     modifier = Modifier
                         .size(18.dp)
                         .clickable { onDismissRequest() },
-                    painter = painterResource(id = R.drawable.close),
-                    contentDescription = "툴팁 종료"
+                    painter = painterResource(id =  R.drawable.close),
+                    contentDescription = "Close Dialog"
                 )
             }
-
         }
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun TooltipDialogPreview() {
-    val viewModel : SignUpCommonViewModel = viewModel()
-    PicplzTheme {
-        NicknameTooltipDialog (
-            onDismissRequest = { viewModel.handleIntent(SignUpCommonIntent.ToggleValidateDialog) }
-        )
-    }
+fun CommonTextDialogPreview() {
+    CommonTextDialog(
+        onDismissRequest = {},
+        dialogText = "∙ 한글, 영문, 숫자 입력 가능 (2~15자)\n∙ 중복 닉네임은 불가\n∙ 이모티콘, 특수문자 사용 불가\n∙ 닉네임의 처음과 마지막 부분 공백 사용 불가",
+        dialogTextSize = 12,
+    )
 }
