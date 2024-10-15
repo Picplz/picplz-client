dfe67f2ed5c90534d40c2fce08204ac3d9ffb121
ldhldh07 <dhlim07@naver.com>
2024-10-15 15:55:00 +0900

    :construction_worker: : 경험 내용 상태 정의 및 기본 리스트 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/SignUpPhotographerState.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/SignUpPhotographerState.kt
@@ -1,7 +1,9 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_photographer
 
+import com.hm.picplz.data.model.ChipItem
 import com.hm.picplz.data.model.PhotographyExperience
 import com.hm.picplz.data.model.User
+import com.hm.picplz.data.model.ChipMode
 import com.hm.picplz.viewmodel.emptyUserData
 
 data class SignUpPhotographerState(
@@ -10,9 +12,20 @@ data class SignUpPhotographerState(
     val error: Throwable? = null,
     val userInfo: User = emptyUserData,
     val hasPhotographyExperience: Boolean? = null,
-    val photographyExperience: PhotographyExperience? = null
+    val photographyExperience: PhotographyExperience? = null,
+    val experienceChipList: List<ChipItem> = defaultChipList()
 ) {
     companion object {
+        private fun defaultChipList(): List<ChipItem> {
+            return listOf(
+                ChipItem(id = "1", label = "MZ 감성"),
+                ChipItem(id = "2", label = "을지로 감성"),
+                ChipItem(id = "3", label = "키치 감성"),
+                ChipItem(id = "4", label = "퇴폐 감성"),
+                ChipItem(id = "5", label = "올드머니 감성")
+            )
+        }
+
         fun idle(): SignUpPhotographerState {
             return SignUpPhotographerState(
                 currentStep = 0,
@@ -20,7 +33,8 @@ data class SignUpPhotographerState(
                 error = null,
                 userInfo = emptyUserData,
                 hasPhotographyExperience = null,
-                photographyExperience = null
+                photographyExperience = null,
+                experienceChipList = defaultChipList()
             )
         }
     }
