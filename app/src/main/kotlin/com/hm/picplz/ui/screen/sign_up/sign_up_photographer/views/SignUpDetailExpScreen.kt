1543df4f457c6541230495f208e25785bdd43ba9
ldhldh07 <dhlim07@naver.com>
2024-10-15 13:48:37 +0900

    :lipstick: : 경험 내용 페이지 레이아웃 설정 - 텍스트 및 칩 영역으로 구분

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpDetailExpScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpDetailExpScreen.kt
@@ -1,18 +1,31 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_photographer.views
 
+import CommonChip
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.hm.picplz.ui.theme.PicplzTheme
 import com.hm.picplz.viewmodel.SignUpPhotographerViewModel
@@ -20,6 +33,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.hm.picplz.MainActivity
+import com.hm.picplz.R
+import com.hm.picplz.ui.screen.common.CommonBottomButton
 import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.*
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerSideEffect
@@ -43,21 +58,82 @@ fun SignUpDetailExpScreen(
     }
     
     val currentState = viewModel.state.collectAsState().value
-    
+
     Scaffold (
-        modifier = Modifier
-            .fillMaxSize(),
-        containerColor =  MainThemeColor.White,
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MainThemeColor.White
     ) { innerPadding ->
         Column (
             modifier = modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             CommonTopBar(
                 text = "경력 선택",
-                onClickBack = { viewModel.handleIntent(NavigateToPrev)}
+                onClickBack = {viewModel.handleIntent(NavigateToPrev)}
             )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 15.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .height(80.dp)
+                    )
+                    Text(
+                        text = "해당되는 경험을 골라주세요.",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(15.dp)
+                    )
+                    Text(
+                        text = "픽플즈는 사진 경력이 없는 금손님도 환영해요!",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(30.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                    }
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                CommonBottomButton(
+                    text = "다음",
+                    onClick = {
+                        if (currentState.hasPhotographyExperience == true) {
+                            viewModel.handleIntent(Navigate("sign-up-detail-experience"))
+                        } else {
+                            /**
+                             * Todo: 경험 내용 스킵하고 자신있는 감성으로 바로 라우팅
+                             * **/
+                        }
+                    },
+                    enabled = currentState.hasPhotographyExperience != null,
+                    containerColor = MainThemeColor.Black
+                )
+            }
         }
     }
 
