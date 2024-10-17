586048608fec858802806d55a143b9ffc94ff806
ldhldh07 <dhlim07@naver.com>
2024-10-17 16:25:49 +0900

    :adhesive_bandage: : 미사용 import문 삭제

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBar.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBar.kt
@@ -19,10 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.hm.picplz.R
 
 @Composable
